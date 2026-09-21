fbdb8d6c0493fd34db4c88b8ef1a4bc91999871f
Talita Lopes <talitabc.lopes@hotmail.com>
2026-09-21 07:12:51 -0300

    Criação das configurações do app

---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,53 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string Cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "76310cc799a4c1fdb2959e77401f5f80";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={Cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if(resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["descriptiom"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                    
+                    }; //Fecha obj do Tempo
+                }// Fecha if se o status do servidor foi de sucesso
+            }// Fecha laço using
+
+
+
+
+            return t;
+        }
+    }
+}
